9d055f40a705e8f85d2a39ebff934054299f44c8
Prateek Madapurmath <prateekm33@gmail.com>
2016-06-09 12:04:18 -0700

    feature for users to choose solo or group Roam // ignore iBeacon components

--- roam/App/Components/Time.js
+++ roam/App/Components/Time.js
@@ -23,26 +23,19 @@ class Time extends Component {
   constructor(props) {
     super(props);
     this.state = {
-<<<<<<< HEAD
-      selectedTime: '1 hour'
-=======
       selectedTime: '1 hour', 
       selectedGroup: 'Solo'
->>>>>>> 1e36fee4bd45e7d45d99a75c0d485ccc63890904
     };
   }
   handleSelectedTime(choice) {
     this.setState({
       selectedTime: choice
-<<<<<<< HEAD
-=======
     });
   }
 
   handleSelectedGroup(choice) {
     this.setState({
       selectedGroup: choice
->>>>>>> 1e36fee4bd45e7d45d99a75c0d485ccc63890904
     });
   }
 
@@ -58,17 +51,16 @@ class Time extends Component {
     });
   }
 
-<<<<<<< HEAD
   handleHistory() {
     console.log('page not built yet!');
-=======
+  }
+
   iBeacon() {
     this.props.navigator.push({
       title: 'iBeacon', 
       email: this.props.navigator.navigationContext._currentRoute.email, 
       component: iBeacon
     })
->>>>>>> 1e36fee4bd45e7d45d99a75c0d485ccc63890904
   }
 
   render () {
@@ -96,10 +88,6 @@ class Time extends Component {
           options={options}
           allowFontScaling={false}
           fontWeight={'bold'}
-<<<<<<< HEAD
-          onSelection={this.handleSelected.bind(this)}
-          selectedOption={this.state.selectedTime} />
-=======
           onSelection={this.handleSelectedTime.bind(this)}
           selectedOption={this.state.selectedTime} />
         <SegmentedControls
@@ -111,24 +99,19 @@ class Time extends Component {
         fontWeight={'bold'}
         onSelection={this.handleSelectedGroup.bind(this)}
         selectedOption={this.state.selectedGroup} />
->>>>>>> 1e36fee4bd45e7d45d99a75c0d485ccc63890904
         <TouchableHighlight
           style={styles.button}
           onPress={this.handleSubmit.bind(this)} >
             <Text style={styles.buttonText}> Roam! </Text>
         </TouchableHighlight>
-<<<<<<< HEAD
         <TouchableHighlight
           style={styles.button}
           onPress={this.handleHistory.bind(this)} >
             <Text style={styles.buttonText}> View History </Text>
-=======
-
         <TouchableHighlight
           style={styles.button}
           onPress={this.iBeacon.bind(this)}>
           <Text>iBeacon</Text>
->>>>>>> 1e36fee4bd45e7d45d99a75c0d485ccc63890904
         </TouchableHighlight>
       </Image>
     );
